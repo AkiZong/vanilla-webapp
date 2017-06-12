47c180dacdc488b26df47a7bb9591b8608811a42
unknown <zbxaki@gmail.com>
2017-06-11 19:43:55 -0700

    2

--- Vanilla/Vanilla/Models/Movie.cs
+++ Vanilla/Vanilla/Models/Movie.cs
@@ -7,5 +7,10 @@ namespace Vanilla.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // this page will be under /movies/random
+
     }
 }
